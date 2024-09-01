a667e70bb9556106c7a0dd2e000057b24312d8fc
affandhanse <affandhanse56@gmail.com>
2024-09-02 01:25:15 +0530

    new urls.py

--- rocketman/urls.py
+++ rocketman/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
-from debug_toolbar.toolbar import debug_toolbar_urls
 from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -16,24 +15,24 @@ urlpatterns = [
     path("search/", search_views.search, name="search"),
 ]
 
-
 if settings.DEBUG:
-    import debug_toolbar
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns = urlpatterns + [
-         path('__debug__/',include(debug_toolbar.urls)),
-    ]
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
+
+    # Debug toolbar
+    try:
+        import debug_toolbar
+        urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    except ImportError:
+        pass
+
+# Add Wagtail URLs at the end
+urlpatterns += [
     path("sitemap.xml", sitemap),
-   
     path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
